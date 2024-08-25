6ce958abbd6f0f89756c906aee7557aad79733fe
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2024-08-25 13:27:56 -0600

    Creacion de la entidad Adoption

---
+++ Domain/Entities/Adoption.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Adoption
+    {
+        public Guid Id { get; set; }
+
+        public Guid AnimalId { get; set; }
+        public Animal? Animal { get; set; }
+
+        public Guid VolunteerId { get; set; }
+        public Volunteer? Volunteer { get; set; }
+
+        public Guid AdopterId { get; set; }
+        public Adopter? Adopter { get; set; }
+
+        public DateTime DateAdoption { get; set; } = DateTime.Now;
+
+
+    }
+}
