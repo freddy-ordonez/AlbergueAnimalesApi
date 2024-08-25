183f1b589869d742c15f9215dccb12bb988252d9
Freddy Ordonez Aguilar <freddy12aguilar@outlook.com>
2024-08-25 13:09:41 -0600

    Creacion de la entidad Animal

---
+++ Domain/Entities/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Animal
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public Type? Type { get; set; }
+        public Estate? Estate { get; set; }
+        public DateTime? DateDelivery { get; set; } = DateTime.Now;
+
+    }
+}
